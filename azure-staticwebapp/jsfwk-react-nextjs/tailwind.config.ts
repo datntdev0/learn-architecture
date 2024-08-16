c09150e0a6cf39687d1d824d52db238265aa1976
Dat Nguyen <datntdev@outlook.com>
2024-08-16 23:30:34 +0700

    jsfwk: configure flowbite ui toolkit

--- azure-staticwebapp/jsfwk-react-nextjs/tailwind.config.ts
+++ azure-staticwebapp/jsfwk-react-nextjs/tailwind.config.ts
@@ -1,20 +1,23 @@
 import type { Config } from "tailwindcss";
+import flowbite from "flowbite-react/tailwind";
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{ts,tsx}",
+    "./public/**/*.html",
+    flowbite.content(),
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+    colors: {
+      primary: {
+        600: '#2563eb',
+        700: '#1d4ed8',
       },
-    },
+    }
   },
-  plugins: [],
+  darkMode: 'media',
+  plugins: [
+    flowbite.plugin(),
+  ],
 };
 export default config;
